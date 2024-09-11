1df3b0473586f49dab2e47c8fda2766b039a971b
PILLIMETLA VAMSI <116826684+pillimetlavamsi@users.noreply.github.com>
2024-09-11 22:48:03 +0530

    Delete duplicate.py

--- duplicate.py
+++
@@ -1,9 +0,0 @@
-def to_find_duplicate(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr)):
-            if arr[i] == arr[j] and i != j:  
-                return arr[i] 
-    return -1 
-arr = [3,1,3,4,2]
-duplicate = to_find_duplicate(arr)
-print("Duplicate Number is:", duplicate)
